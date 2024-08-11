b32e887c55fe58178e21586a0e40dff5e933f558
senseImamuraY <89624108+senseImamuraY@users.noreply.github.com>
2024-08-11 20:06:39 +0900

    fin ch 134

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -4,6 +4,8 @@
 #include "Tower.h"
 #include "Tank.h"
 #include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+
 
 ATower::ATower()
 {
@@ -13,14 +15,8 @@ ATower::ATower()
 void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
-	if (!Tank) return;
 
-	float DistToTank = FVector::Dist(Tank->GetActorLocation(), this->GetActorLocation());
-
-	UE_LOG(LogTemp, Log, TEXT("SomeValue: %f"), DistToTank);
-
-	if (DistToTank <= FireRange) 
+	if (InFireRange()) 
 	{
 		RotateTurret(Tank->GetActorLocation());
 	}
@@ -32,4 +28,31 @@ void ATower::BeginPlay()
 
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
 
+	GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
 }
+
+void ATower::CheckFireCondition()
+{
+	if (InFireRange()) 
+	{
+		DrawDebugSphere(
+			GetWorld(),
+			Tank->GetActorLocation(),
+			25.f,
+			12,
+			FColor::Blue,
+			false,
+			2.f);
+	}
+}
+
+bool ATower::InFireRange()
+{
+	if (!Tank) return false;
+
+	float DistToTank = FVector::Dist(Tank->GetActorLocation(), this->GetActorLocation());
+	if (FireRange >= DistToTank) return true;
+	
+	return false;
+}
+
